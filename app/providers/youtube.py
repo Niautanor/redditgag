1ced7d5c6416abe813b95a1ade6ff5da93cfe27d
Tim Malte Gräfje <tim.graefje@gmx.de>
2016-09-08 14:35:23 +0200

    added youtube provider
    playlists and timestamps don't work currently but whatever. I can do that later

--- app/providers/youtube.py
+++ app/providers/youtube.py
@@ -1,2 +1,21 @@
+"""
+This module is responsible for embedding youtube videos.
+
+Like with most of my embeddable providers, portions of the code (regexes in
+particular) come from the Reddit Enhancement Suite
+"""
+
+import re
+
+youtube_regex = re.compile(r'^https?:\/\/(?:youtu\.be|(?:www\.|m\.)?youtube\.com)\/?(?:watch|embed)?(?:.*\?v=|v\/|\/)([\w-]+)', re.IGNORECASE)
+
 def embed(submission):
+    match = youtube_regex.match(submission.url)
+    if match is not None:
+        # TODO: detect timestamps (&t=1m30s), start, end and list in the url and
+        # handle them appropriately
+        return {
+            'kind' : 'IFRAME',
+            'url' : "https://www.youtube.com/embed/%s" % match.group(1)
+        }
     return None
