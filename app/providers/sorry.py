2d2aac12ae4980394e1b96d3a4f3066a18b80f61
Tim Malte Gräfje <Tim.Graefje@gmx.de>
2016-10-09 19:28:50 +0200

    fix minor errors in unsupported links

--- app/providers/sorry.py
+++ app/providers/sorry.py
@@ -12,12 +12,12 @@ Maybe someone will send the developer a pull request to fix this ;)
 # contains a mapping of domains to a set of urls (belonging to this domain)
 # we can later count the urls and so compute the number of unresolved requests
 # to this domain
-unembedded_domains = dict()
+unembedded_domains = {}
 
 def embed(submission):
     domain = urlparse(submission.url).netloc
 
-    # default value will be the empty set. 
+    # default value will be the empty set.
     unembedded_domains[domain] = unembedded_domains.get(domain, set())
     unembedded_domains[domain].add(submission.url)
 
@@ -28,7 +28,8 @@ def embed(submission):
 
 # sort the dict by values and returns it
 def get_unsupported_domains():
-	counted_domains = dict()
-	for domain in unembedded_domains.keys():
-		counted_domains[domain] = len(unembedded_domains[domain])
-	return reversed(sorted(counted_domains.items(), key=operator.itemgetter(1)))
+    counted_domains = {}
+    for domain, links in unembedded_domains.items():
+        counted_domains[domain] = len(links)
+    return reversed(sorted(counted_domains.items(),
+                           key=operator.itemgetter(1)))
