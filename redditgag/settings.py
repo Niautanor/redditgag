38dfb82d252cf7f751b392f63eb0c5a2c4636331
Tim Malte Gräfje <Tim.Graefje@gmx.de>
2016-10-15 23:12:01 +0200

    configure compilestatic command correctly
    Bower decided that it should include source files (less files in
    bootstrap particular) in the packages it downloads. Since
    django-static-precompiler's compilestatic looks through all available
    static files (which includes bower_components that were installed by
    django-bower), it tries to compile bootstraps less files which fails
    because I don't have a less compiler installed.
    
    To fix this, I have configured the list of available compilers manually
    but ideally, I would like to have an option in django-static-precompiler
    to ignore certain directories (like bower_components). I don't know how
    possible this is though.

--- redditgag/settings.py
+++ redditgag/settings.py
@@ -18,11 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# redditgag/secret.py defines options that differ between development and
-# production environments. An example (that you can copy / symlink to the right
-# place) is given in secret_example.py
-from .secret import SECRET_KEY, DEBUG, ALLOWED_HOSTS, SECURE_SSL_REDIRECT, STATIC_ROOT
-
 # django.middleware.security.SecurityMiddleware settings
 # those were basically obtained by running `python manage.py check --deploy` and
 # setting all the values it complained about.
@@ -111,11 +106,36 @@ USE_L10N = True
 
 USE_TZ = True
 
+# redditgag/secret.py defines options that differ between development and
+# production environments. An example (that you can copy / symlink to the right
+# place) is given in secret_example.py
+from .secret import (
+        SECRET_KEY,
+        DEBUG,
+        ALLOWED_HOSTS,
+        SECURE_SSL_REDIRECT,
+        STATIC_ROOT,
+        STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE,
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# this project uses Coffee script and Handlebars and only needs those compilers.
+# If this setting is left at the default, static_precomipler tries to compile
+# less files from bowers version of bootstrap (bower shouldn't even download
+# those files but that is another issue)
+STATIC_PRECOMPILER_COMPILERS = (
+    'static_precompiler.compilers.CoffeeScript',
+    'static_precompiler.compilers.Handlebars',
+)
+
+# collect compiled files (with collectstatic) only if we can't compile them on
+# the fly (in that case it is assumed that we have run compilestatic before
+# collectstatic)
+STATIC_PRECOMPILER_FINDER_LIST_FILES = STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE
 
 # django-static-precompiler will compile to this directory
 STATIC_PRECOMPILER_ROOT = os.path.join(BASE_DIR, 'static_precompiler')
--- secret_example.py
+++ secret_example.py
@@ -11,8 +11,11 @@ ALLOWED_HOSTS = []
 # If you have SSL, you should probably set this to True
 SECURE_SSL_REDIRECT = False
 
-
 # Django's collectstatic command will put static files here. In a production
 # environment, this should point to a directory that is served by your webserver
 # under the STATIC_URL
 STATIC_ROOT = None
+
+# You might want to set this to True in a production environment if you don't
+# want to / can't give the process that runs django access to your STATIC_ROOT
+STATIC_PRECOMPILER_DISABLE_AUTO_COMPILE = False
